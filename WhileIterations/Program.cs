371dced4d2fb5a8ed3dbd991e776f8784a47eaa6
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-10-22 18:04:35 -0700

    Add files via upload
    User is given a menu and may choose from 2 games, or exit. Practicing calling methods, loops, and different options in console output in C# as compared to Java.

---
+++ WhileIterations/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace WhileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Print Numbers");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
+            int result = int.Parse(Console.ReadLine());
+
+            switch (result)
+            {
+                case 1: PrintNumbers();
+                    break;
+                case 2: GuessingGame();
+                    break;
+                case 3: break;
+                default:
+                    Console.WriteLine("That won't work");
+                    break;
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print Numbers!");
+            Console.Write("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter <= result)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guessing Game!");
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                string result = Console.ReadLine();
+                guesses++;
+
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses", guesses);
+
+            Console.ReadLine();
+        }
+    }
+}
