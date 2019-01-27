ef87bfcf109f4e539988cc59f24904e7f87ab276
actualshovel <40729236+actualshovel@users.noreply.github.com>
2019-01-27 12:51:04 -0800

    Add files via upload

--- Reverse Integer/Program.cs
+++ Reverse Integer/Program.cs
@@ -1,1 +1,47 @@
-https://puu.sh/CDeH8/9d3d77d7ed.png
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to be reversed: ");
+            int number = Convert.ToInt32( Console.ReadLine() );
+            
+            Console.WriteLine( Reverse(number) );
+            Console.ReadLine();
+        }
+
+        public static int Reverse(int x)
+        {   
+            //will need StringBuilder here
+            StringBuilder sb = new StringBuilder("");
+            //add parameter to sb
+            sb.Append(x.ToString());
+
+            //check for '-' char
+            if (sb.ToString().Contains("-") )
+            {
+                sb.Remove(0, 1);
+                sb.Append('-');
+            }
+            
+            string answer = "";
+            //decrement from last index to reverse string
+            for (int i = sb.Length - 1; i >= 0; i--)
+            {
+                answer += sb[i];
+            }
+            //need to return as int
+            int result;
+            try {
+                result = int.Parse(answer, System.Globalization.NumberStyles.AllowLeadingSign); }
+            catch(OverflowException e) {
+                return 0;
+            }
+            return result;
+            }
+    }
+}
