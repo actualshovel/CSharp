ea3e28652e6bf354312249df504419e9f0022309
domFromCA <40729236+domFromCA@users.noreply.github.com>
2019-02-11 21:46:44 -0800

    Add files via upload

--- FibonacciSequence/Fibonacci.cs
+++ FibonacciSequence/Fibonacci.cs
@@ -1,1 +1,32 @@
+﻿//Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+using System;
 
+namespace FibonacciSequence
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number\n>");
+            int userInput = int.Parse(Console.ReadLine());
+
+            TheSequence(userInput);
+
+            Console.ReadLine();
+        }
+
+        private static void TheSequence(int x)
+        {
+            int result = 0;
+            int a = 0, b = 1, c = 1;
+            while (result < x)
+            {
+                Console.Write(c + ", ");
+                result = b+c;
+                a=b;
+                b=c;
+                c=result;
+            }
+        }
+    }
+}
