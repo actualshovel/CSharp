fc3ad9a2e286b84417d17e67cda5e7b38b7c4d7b
actualshovel <40729236+actualshovel@users.noreply.github.com>
2019-02-22 19:23:31 -0800

    update both classes

--- UnitConverter/UnitConverter/Program.cs
+++ UnitConverter/UnitConverter/Program.cs
@@ -1,9 +1,6 @@
 ﻿//Unit Converter(temp, currency, volume, mass and more) - 
 //Converts various units between one another.The user enters the type of unit being entered, 
 //the type of unit they want to convert to and then the value.The program will then make the conversion.
-
-
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,41 +17,27 @@ namespace UnitConverter
         static void Main(string[] args)
         {
             //TODO: wrap within a loop
-            string selectUnit = "";
-            string userValue = "";
+            double userTemperature = 0;
+            string userChoice;
             string answer = "";
 
             //user enters type of unit to be converted
             Console.WriteLine("What unit are you converting from?");
             Console.WriteLine("ex: temp, currency, volume, mass");
-            selectUnit = Console.ReadLine();
-            
-            //20FEB2019
-            //WHAT I NEED
-            //allow user to type (ex. "68F" to represent 68 degrees fahrenheit
-            //determine if there is an F or a C
-            //convert depending on which (char? substring?) is used
-            if (selectUnit.ToLower().Equals("temp"))
+            userChoice = Console.ReadLine();
+
+            if (userChoice.ToLower().Equals("temp"))
             {
                 
-                Console.WriteLine("Enter the temp followed by a 'C' for celsius or an 'F' for fahrenheit:");
-                userValue = Console.ReadLine();
-                Temperature tmp = new Temperature(userValue);
-                answer = tmp.ToFahrenheit.ToString();
+                Console.WriteLine("Enter the temperature:");
+                answer = Console.ReadLine();
+                Temperature tmp = new Temperature(answer);
+
+                Console.WriteLine(tmp.FahrenheitToCelsius());
+                
 
             }
-            
-            Console.WriteLine("Converted {0} to {1}!", userValue, answer);
-
             Console.ReadLine();
         }
-
-
-
-        //static string CheckType(string unit)
-        //{
-        //    string temp = unit.Substring(unit.Length-1);
-        //    return temp;
-        //}
     }
 }
