9410bae501dfca37fb5d769c5f947fab308d51aa
shovellware <40729236+shovellware@users.noreply.github.com>
2019-03-05 18:56:59 -0800

    Update Program.cs

--- UnitConverter/UnitConverter/Program.cs
+++ UnitConverter/UnitConverter/Program.cs
@@ -1,6 +1,4 @@
-﻿//Unit Converter(temp, currency, volume, mass and more) - 
-//Converts various units between one another.The user enters the type of unit being entered, 
-//the type of unit they want to convert to and then the value.The program will then make the conversion.
+﻿//Unit Converter(temp, currency, volume, mass and more)
 using System;
 using System.Collections.Generic;
 using System.Linq;
