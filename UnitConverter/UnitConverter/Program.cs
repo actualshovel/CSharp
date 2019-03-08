c67a27da94a1c37a2a7e7bf01fa25e580d32222a
trmye <trmye@microsoft.com>
2019-03-08 12:15:47 -0800

    No major changes... some minor comments and thoughts

--- UnitConverter/UnitConverter/Program.cs
+++ UnitConverter/UnitConverter/Program.cs
@@ -7,8 +7,23 @@ namespace UnitConverter
         //TODO: main program will loop
         static void Main(string[] args)
         {
+            // Overall design, how could you write a menu function
+            // that would allow you to re-use your menu?
+            // Example would be, your main menu and temperature
+            // menu would use the exact same calls...
+
+            // Super common use of the 'menu' loop, which is a while loop
+            // You know a menu is going to be presented at least once
+            // no matter the circumststance, so execute the block
+            // and check what the user selected after doing it at
+            // least once. By adding the selection 'q'
+            // I'm relying on the user for exit conditions
+
             //TODO: improve design of menus and output text
             //select what kind of conversions to be made
+            // Add a writeline for description
+            // Add a third option, 'q' for quit to exit program
+            // Add a description of what to do, similar to TempMenu()
             Console.WriteLine("1) Temperature\n" +
                               "2) Weight\n" +
                               "3) Distance");
@@ -16,6 +31,7 @@ namespace UnitConverter
             //user needs to only choose 1, 2, or 3
             //so a switch statement is enough here to execute the single
             //line of code that takes care of the desired conversion
+            // Add your quit case to exit the program
             int unitChoice = int.Parse(Console.ReadLine());
             switch(unitChoice)
             {
@@ -46,6 +62,8 @@ namespace UnitConverter
             double userTemp = double.Parse(Console.ReadLine());
 
             Temperature temp = new Temperature();
+
+            // Why not user your switch here?
             if (choice == 1)
             {
                 temp.Fahrenheit = userTemp;
--- UnitConverter/UnitConverter/Temperature.cs
+++ UnitConverter/UnitConverter/Temperature.cs
@@ -6,20 +6,49 @@ namespace UnitConverter
     //user will then enter a temperature to be converted by the appropriate method
     class Temperature
     {
+        // We know we're going to convert this value to Celsius,...
+        // When you set Fahrenheit, this is the perfect time to
+        // automatically set Celsius.
         private double _fahrenheit;
-        public double Fahrenheit { get; set; }
+
+        // Keep in mind, this would only be useful if you wanted to keep
+        // track of multiple instances of temperature over time
+        public double Fahrenheit
+        {
+            get
+            {
+                return _fahrenheit;
+            }
+
+            set
+            {
+                _fahrenheit = value;
+                _celsius = (value - 32) * 5 / 9;
+            }
+        }        
 
         private double _celsius;
         public double Celsius { get; set; }
+
+
+        // Technique for commenting code in VS, right above the method
+        // type in /// and then press tab. This will auto-comment
+        // including parameters if the method has any
+        // Only summarize if absolutely necessary. The method name
+        // should ideally let a developer know what it does.
 
         //formula to get fahrenheit given celsius
         public void GetFahrenheit()
         {
             Fahrenheit = (Celsius * (9/5) + 32);
-            Console.WriteLine("{0:00}C is equal to {1:00}F", Celsius, Fahrenheit);
+            // Perfectly fine passing text information, but you may want to make this information
+            // easier to use and modify.
+            Console.WriteLine("{0:00}C is equal to {1:00}F", Celsius, Fahrenheit); // Good use of the formatting!!
         }
 
-        //formula to get celsius given fahrenheit
+        /// <summary>
+        /// This is auto-generated, then I added some text to the line.
+        /// </summary>
         public void GetCelsius()
         {
             Celsius = (Fahrenheit - 32) * 5 / 9;
