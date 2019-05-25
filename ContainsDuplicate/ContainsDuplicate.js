2167c9eee58f2d2d57fe4ae183e313dc55791828
shovellware <40729236+shovellware@users.noreply.github.com>
2019-05-25 14:15:44 -0700

    Add files via upload

--- ContainsDuplicate/ContainsDuplicate.js
+++ ContainsDuplicate/ContainsDuplicate.js
@@ -1,1 +1,19 @@
+var checkForDupes = function(nums) {
+    // There cannot be duplicates if there is one 1 or less values
+    if (nums.length < 2) {
+        return false;
+    }
 
+    // Create a set - only unique values. put the array into it
+    var thisSet = new Set(nums);
+
+    // If the set length is smaller than array length, we have lost some values
+    // that were repeated in the array
+    if (thisSet.size < nums.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+var testArray = [1,2,3,1];
+console.log(checkForDupes(testArray));
