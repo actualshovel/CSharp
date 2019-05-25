8c4bf505f33f25620cdce43c4b614cdf935ace60
shovellware <40729236+shovellware@users.noreply.github.com>
2019-05-25 16:09:07 -0700

    Add files via upload

--- SortByParity/SortArrayByParity.js
+++ SortByParity/SortArrayByParity.js
@@ -1,1 +1,19 @@
+var sortArrayByParity = function(A) {
+    var result = [];
 
+    // Put all even numbers into my new array, and remove them from the original
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] % 2 == 0) {
+            result.push(A[i])
+            A.splice(i, 1);
+            i--;
+        }
+    }
+    // Add the suriving numbers from original array to the end of the new array
+    // push() adds to the end of result, and apply() will add the individual members from A[]
+    result.push.apply(result, A);
+    return result;
+};
+
+var input = [3,1,2,4];
+console.log(sortArrayByParity(input));
