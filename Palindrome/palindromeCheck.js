9572b203e9ee2ae2dd6af8c87b97f3cd9237bc6d
shovellware <40729236+shovellware@users.noreply.github.com>
2019-07-23 10:16:08 -0700

    Add files via upload

--- Palindrome/palindromeCheck.js
+++ Palindrome/palindromeCheck.js
@@ -1,1 +1,13 @@
+function palindrome(str) {
+  let myReg = /[a-z0-9]/ig;
+  let myArr =  str.toLowerCase().match(myReg);
+  console.log(myArr)
 
+  for (let i = 0; i < myArr.length/2; i++) {
+    if (myArr[i] != myArr[myArr.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(palindrome("0_0 (: /-\ :) 0-0"));
